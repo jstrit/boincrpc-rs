6cff10d423f759569fc32d61f3243fa147ba1355
John <jstritenberger@pentagear.com>
2018-02-25 12:42:38 -0500

    Improve basic xml parser

--- src/rpc/xml.rs
+++ src/rpc/xml.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use std::fmt;
+use std::marker::Sized;
 
 // b"<boinc_gui_rpc_request>\n<get_state/>\n</boinc_gui_rpc_request>\n\x03"
 
@@ -24,42 +25,50 @@ impl XmlTags {
     }
 }
 
+trait GetElement<'a> {
+    fn get_xml_text(&self) -> &'a str;
+
+    fn get_element(&'a self, element_name: &'a str) -> Result<XmlElement, String> {
+        match self.extract_element_text(element_name) {
+            Ok(element_text) => Ok(XmlElement::new(element_text)),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn extract_element_text(&self, element_name: &'a str) -> Result<&'a str, String> {
+        let tags = XmlTags::new(element_name);
+        let xml_text = self.get_xml_text();
+        match xml_text.find(tags.open()) {
+            Some(start_index) => match xml_text.find(tags.close()) {
+                Some(end_index) => Ok(&xml_text[start_index..end_index + tags.close().len()]),
+                None => Err(format!("closing tag {0} not found.", tags.close())),
+            },
+            None => Err(format!("opening tag {0} not found.", tags.open())),
+        }
+    }
+}
+
 // XmlElement
 struct XmlElement<'a> {
     name: &'a str,
-    text: &'a str,
+    xml_text: &'a str,
+}
+
+impl<'a> GetElement<'a> for XmlElement<'a> {
+    fn get_xml_text(&self) -> &'a str {
+        self.xml_text
+    }
 }
 
 impl<'a> XmlElement<'a> {
-    pub fn new(text: &'a str) -> XmlElement {
-        let name = &text[text.find("<").unwrap() + 1..text.find(">").unwrap()];
-        XmlElement { name, text }
+    pub fn new(xml_text: &'a str) -> XmlElement {
+        let name = &xml_text[xml_text.find("<").unwrap() + 1..xml_text.find(">").unwrap()];
+        XmlElement { name, xml_text }
     }
-    pub fn get_element(&self, name: &str) -> Result<XmlElement, String> {
-        let tags = XmlTags::new(name);
-        match self.text.find(tags.open()) {
-            Some(start_index) => match self.text.find(tags.close()) {
-                Some(end_index) => Ok(XmlElement::new(&self.text[start_index..end_index + tags.close().len()])),
-                None => Err(format!("closing tag {0} not found.", tags.close())),
-            },
-            None => Err(format!("opening tag {0} not found.", tags.open())),
-        }
-    }
-    pub fn get_value<T>(&self) -> Result<T, String>
-    where
-        T: FromStr,
-        T::Err: fmt::Debug
-    {
-        let tags = XmlTags::new(self.name);
-        match self.text.find(tags.open()) {
-            Some(start_index) => match self.text.find(tags.close()) {
-                Some(end_index) => Ok(T::from_str(
-                    &self.text[start_index + tags.open().len()..end_index],
-                ).unwrap()),
-                None => Err(format!("closing tag {0} not found.", tags.close())),
-            },
-            None => Err(format!("opening tag {0} not found.", tags.open())),
-        }
+    pub fn get_value(&self) -> &'a str {
+        let element_text = self.extract_element_text(self.name)
+            .expect("malformed element");
+        &element_text[element_text.find('>').unwrap() + 1..element_text.rfind('<').unwrap()]
     }
 }
 
@@ -77,7 +86,14 @@ mod tests {
              </outer_element>\n",
         );
 
-        let string_element: String = xml.get_element("string_element").unwrap().get_value().unwrap();
-        assert_eq!("string", string_element.as_str());
+        // Element.
+        let string_value = xml.get_element("string_element").unwrap().get_value();
+        assert_eq!("string", string_value);
+
+        // Nested element.
+        let outer_element = xml.get_element("outer_element").unwrap();
+        let inner_element = outer_element.get_element("inner_element").unwrap();
+        let inner_value = inner_element.get_value();
+        assert_eq!("inner", inner_value);
     }
 }
